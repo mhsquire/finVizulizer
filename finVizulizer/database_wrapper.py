505d0c2c8332b1def6275b518cf41950e6f8734b
Matt Squire <mhsquire83@gmail.com>
2022-06-17 06:48:42 -0600

    added wrapper to make databases interchangeable.

--- finVizulizer/database_wrapper.py
+++ finVizulizer/database_wrapper.py
@@ -14,3 +14,12 @@ class DatabaseWrapper:
 
     def put_ticker(self, ticker_name, data):
         self.db.insert(ticker_name, data)
+
+    def remove_ticker(self, ticker_name):
+        self.db.delete(ticker_name)
+
+    def read_ticker(self, ticker_name):
+        return self.db.read(ticker_name)
+
+    def update_ticker(self, ticker, new_data):
+        self.db.update(ticker, new_data)
--- tests/unit_tests/test_database_wrapper.py
+++ tests/unit_tests/test_database_wrapper.py
@@ -9,17 +9,47 @@ def database_wrap():
 
 
 @pytest.fixture
-def stock1_data():
+def stock_data1():
     return {
         "name": "tesla",
         "ticker": "TSLA"
     }
 
+@pytest.fixture
+def stock_data2():
+    return {
+        "name": "tesla",
+        "ticker": "TSLAHH"
+    }
+
+@pytest.fixture
+def stocked_database(database_wrap, stock_data1):
+    db = database_wrap
+    db.db.stocks["TSLA"] = stock_data1
+    return db
+
 
 class TestDatabaseWrapper:
 
-    def test_put_ticker(self, database_wrap, stock1_data):
+    def test_put_ticker(self, database_wrap, stock_data1):
         ticker = "TSLA"
         db = database_wrap
-        db.put_ticker(ticker, stock1_data)
-        assert db.db.read(ticker) == stock1_data
+        db.put_ticker(ticker, stock_data1)
+        assert db.db.read(ticker) == stock_data1
+
+    def test_remove_ticker(self, stocked_database):
+        ticker = "TSLA"
+        db = stocked_database
+        db.remove_ticker(ticker)
+        assert db.db.read(ticker) == None
+
+    def test_read_ticker(self, stocked_database, stock_data1):
+        ticker = "TSLA"
+        db = stocked_database
+        assert stock_data1 == db.read_ticker(ticker)
+
+    def test_update_ticker(self, stocked_database, stock_data2, stock_data1):
+        ticker = "TSLA"
+        db = stocked_database
+        db.update_ticker(ticker, stock_data2)
+        assert stock_data2 == db.db.read(ticker)
