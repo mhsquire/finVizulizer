ebd94ad5fac9253a9e5ae13c59abf14c14b2ca3d
Matt Squire <mhsquire83@gmail.com>
2022-06-18 10:03:41 -0600

    added logging to classes that are currently developed.

--- finVizulizer/request_wrapper.py
+++ finVizulizer/request_wrapper.py
@@ -1,11 +1,14 @@
 import requests
+from finVizulizer.setup_logger import logger
 
 
 class RequestWrapper:
 
     def __init__(self):
         self.headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:20.0) Gecko/20100101 Firefox/20.0'}
+        logger.debug(f"request_wrapper initialized.")
 
     def get(self, url):
-
-        return requests.get(url, headers=self.headers)
+        response = requests.get(url, headers=self.headers)
+        logger.debug(f"Response from {url} is {response.status_code}")
+        return response
