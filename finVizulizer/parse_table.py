3ab15685ddf2ec320d6a546ebae6137b274ca28d
Matt Squire <mhsquire83@gmail.com>
2022-06-23 07:48:19 -0600

    committing to save in the cloud. working unit tests. need to finish acceptance test.

--- finVizulizer/parse_table.py
+++ finVizulizer/parse_table.py
@@ -1,6 +1,40 @@
+from bs4 import BeautifulSoup
+from finVizulizer.setup_logger import logger
 
 
 class ParseTable:
 
-    def __init__(self):
-        pass
+    @classmethod
+    def read_html(cls, response):
+        parsed_response = BeautifulSoup(response, "html.parser")
+        return parsed_response
+
+    @classmethod
+    def find_news(cls, parsed_response: BeautifulSoup):
+        table = parsed_response.find(id="news-table")
+        if not table:
+            logger.error("Can't find news of ticker.")
+            return None
+        return table
+
+    @classmethod
+    def parse_news(cls, html_table: BeautifulSoup):
+        parsed_data = []
+        date = ""
+        for row in html_table.findAll("tr"):
+
+            datetime = row.td.text.split()
+            if len(datetime) > 1:
+                date = datetime[0]
+                time = datetime[1]
+            else:
+                time = datetime[0]
+
+            headline = row.a.text
+            comma_headline = headline.split(',')
+            if len(comma_headline) > 1:
+                headline = "\\,".join(comma_headline)
+
+            link = row.find("a").get("href")
+            parsed_data.append(f'{date},{time},{headline},{link}')
+        return parsed_data
--- tests/step_definitions/test_main.py
+++ tests/step_definitions/test_main.py
@@ -1,5 +1,8 @@
-from pytest_bdd import when, then, scenarios
+import re
+
+from pytest_bdd import given, when, then, scenarios
 from finVizulizer import request_wrapper as rw
+from finVizulizer.parse_table import ParseTable as pt
 from tests.helpers.config import finwiz_url
 
 scenarios("../features/main.feature")
@@ -14,3 +17,41 @@ def response_received(context):
 @then("connection is ok")
 def connection_is_ok(context):
     assert(context["response"].status_code == 200)
+
+
+@given("a response has been sent", target_fixture="response")
+def a_response_has_been_sent():
+    return 200, open("tests/helpers/finviz_page.html").read()
+
+
+@when("the response is parsed", target_fixture="news")
+def the_response_is_parsed(response):
+    _, html = response
+    parsed_html = pt.read_html(html)
+    news = pt.find_news(parsed_html)
+    return pt.parse_news(news)
+
+
+@then("the resulting table records date")
+def the_resulting_table_records_date(news):
+    # matches 3 letters (-) 2 numbers (-) 2 numbers
+    date = re.compile("^\w{3}-\d{2}-\d{2}")
+    for article in news:
+        comma_article = article.split(",")
+        print(comma_article[0])
+        assert date.match(comma_article[0])
+
+
+@then("the resulting table records time")
+def step_impl():
+    raise NotImplementedError(u'STEP: And the resulting table records time')
+
+
+@then("the resulting table records headline")
+def step_impl():
+    raise NotImplementedError(u'STEP: And the resulting table records headline')
+
+
+@then("the resuting table records article link")
+def step_impl():
+    raise NotImplementedError(u'STEP: And the resuting table records article link')
